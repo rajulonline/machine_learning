13fe3fe97556b076786a2734198be4fefa9a3d71
rajulonline <rajulonline@gmail.com>
2017-04-13 18:14:04 -0700

    iris dataset classification using KNN

---
+++ iris_data.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+from IPython.display import display
+
+
+#import the iris data set
+from sklearn.datasets import load_iris
+iris_dataset = load_iris()
+
+print("Keys of iris_dataset: \n{}".format(iris_dataset.keys()))
+print(iris_dataset['DESCR'][:193] + "\n...")
+print("Target names: {}".format(iris_dataset['target_names']))
+
+
+#split the dataset into training data and test data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+#do a basic scatter plot to find out what algorithms could be applied.
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+grr = pd.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o', hist_kwds={'bins':20}, s=60, alpha=.8, cmap=mglearn.cm3)
+
+
+#KNNeighbors Classification is the easiest and fits our requirement
+#Train the model
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1) #looking for just one neighboring point in the cluster. 
+knn.fit(X_train, y_train)
+print(knn)
+
+
+
+
+#Sample test the model outcome using a hardcoded test array data
+X_new = np.array([[5,2.9,1,0.2]])
+print("X_new.shape: {}".format(X_new.shape))
+prediction = knn.predict(X_new)
+print(prediction)
+print("prediction: {}".format(prediction))
+print("Predicted target name: {}".format(iris_dataset['target_names'][prediction]))
+
+
+
+#Evaluating the model using our test data set
+y_pred = knn.predict(X_test)
+print("Test set predictions:\n {}".format(y_pred))
+print("Test set score: {:.2f}".format(np.mean(y_pred == y_test)))
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
